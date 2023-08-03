1fc919645cff4b3b2dfcf2f500294bc8b7014349
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-08-02 21:14:49 -0500

    Add dark theme functionality, limit synonyms

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,38 @@ import { ReactComponent as MagnifyingGlass } from "./assets/iconoir_search.svg";
 
 function App() {
 
-  const [word, setWord] = useState("compute");
+  const [word, setWord] = useState("cat");
   const [meanings, setMeanings] = useState([]);
   const [phonetics, setPhonetics] = useState([]);
+  const [synonyms, setSynonyms] = useState([]);
+  const [source, setSource] = useState("");
   const inputRef = useRef(null)
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         if (!response.ok) {
-          throw new Error('Error fetching data');
+          setError('Word not found');
+          return;
         }
 
         const data = await response.json();
         const searchedWord = data[0].word;
         const meanings = data[0].meanings;
         const phonetics = data[0].phonetics;
+        const synonyms = data[0].meanings[0].synonyms;
+        const source = data[0].sourceUrls[0];
 
+        setError(null);
         setWord(searchedWord);
         setMeanings(meanings);
         setPhonetics(phonetics);
-
+        setSynonyms(synonyms);
+        setSource(source);
       } catch (error) {
-        console.log(error);
+        setError('An error occured while fetching data');
       }
     };
     fetchData();
@@ -41,6 +49,25 @@ function App() {
   function changeWord(event) {
     event.preventDefault();
     setWord(inputRef.current.value);
+  }
+
+  // function changeWordHandler(newWord) {
+  //   setWord(newWord);
+  // }
+
+  function playSound() {
+    const audio = new Audio(phonetics[0].audio);
+    audio.play();
+  }
+
+  function changeTheme() {
+    const definitionsList = document.querySelectorAll(".definition");
+    const root = document.documentElement;
+    root.classList.toggle("dark");
+    definitionsList.forEach(definition => {
+      definition.classList.toggle("dark-text");
+    });
+
   }
 
 
@@ -54,14 +81,14 @@ function App() {
             <div className="font-name hover:text-purple cursor-pointer">Sans Serif</div>
             < FontSelector className="ml-3" />
           </div>
-          <div className="theme-switch-group">
+          <button className="theme-switch-group" onClick={changeTheme}>
             < DarkThemeIcon className="dark-theme-icon" />
-          </div>
+          </button>
         </div>
 
         {/* Search Bar */}
-        <form onSubmit={changeWord} className="search p-3 px-6 rounded-full flex items-center justify-between bg-pale my-6">
-          <input ref={inputRef} type="text" placeholder="search" className="search-input bg-pale" />
+        <form onSubmit={changeWord} className="search p-3 px-6 rounded-full flex items-center justify-between bg-pale my-6 gap-8 light-text">
+          <input ref={inputRef} type="text" placeholder="search" className="search-input bg-pale grow" />
           <button className="search-button" type="submit">
             <MagnifyingGlass className="search-icon" />
           </button>
@@ -70,51 +97,72 @@ function App() {
 
       {/* Main Content */}
       <section className="contents">
-        <div className="result-word-container flex items-center justify-between my-7">
-          <div className="result-word flex-col space-y-2">
-            <div className="word font-bold text-4xl font-lora">{word}</div>
-            <div className="pronunciation font-inter text-purple text-2xl">{phonetics.length > 0 ? phonetics[0].text : ''}</div>
-          </div>
-          <div className="play-sound">
-            < PlayButton className="play-icon cursor-pointer rounded-full" />
-          </div>
-        </div>
-        <div className="meaning-container">
-
-          {/* Part of Speech */}
-          {meanings.map((meaningGroup, index) => (<div className="holder" key={index}>
-            <div className="part-of-speech-container flex mb-5" >
-              <div className="part-of-speech font-inter text-black text-lg mb-2 italic font-bold">{meaningGroup.partOfSpeech}
+        {error ? (
+          <div className="error-message text-center font-inter text-3xl font-bold text-lightGrey">{error}</div>
+        ) : (
+          <>
+            <div className="result-word-container flex items-center justify-between my-7">
+              <div className="result-word flex-col space-y-2">
+                <div className="word font-bold text-4xl font-lora">{word}</div>
+                <div className="pronunciation font-inter text-purple text-2xl">{phonetics.length > 0 ? phonetics[0].text : ''}</div>
               </div>
-              <div className="line border-t-2 w-full mt-3 ml-3"></div>
+
+              {/* Audio Button */}
+              <button className="play-sound" onClick={playSound}>
+                < PlayButton className="play-icon cursor-pointer rounded-full" />
+              </button>
+            </div>
+            <div className="meaning-container">
+
+              {/* Part of Speech */}
+              {meanings.map((meaningGroup, index) => (<div className="holder" key={index}>
+                <div className="part-of-speech-container flex mb-5" >
+                  <div className="part-of-speech font-inter text-black text-lg mb-2 italic font-bold">{meaningGroup.partOfSpeech}
+                  </div>
+                  <div className="line border-t-2 w-full mt-3 ml-3"></div>
+                </div>
+
+                {/* Meaning */}
+                <div className="meaning-content">
+                  <div className="meaning-header font-inter font-regular text-lightGrey mb-3">Meaning</div>
+                  <ul className="meanings list-disc font-inter font-regular text-lg leading-7 mb-5 ml-5 text-darkGrey" id="definitions">
+                    {meaningGroup.definitions.slice(0, 5).map((definition, index) => (
+                      <li className="def mb-3 dark-text" key={index}>{definition.definition}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              ))}
+
+              {/* Synonyms */}
+              <div className="synonyms-container flex items-left gap-7 py-3">
+                <div className="synonyms-header font-inter font-regular text-lightGrey mb-3">Synonyms</div>
+                <div className="synonyms font-inter text-purple font-bold">
+                  {synonyms.length > 0 ? synonyms.slice(0, 5).join(', ') : ''}
+
+                </div>
+              </div>
             </div>
 
-            {/* Meaning */}
-            <div className="meaning-content">
-              <div className="meaning-header font-inter font-regular text-lightGrey mb-3">Meaning</div>
-              <ul className="meanings list-disc font-inter font-regular text-lg leading-7 mb-5 ml-5 text-darkGrey">
-                {meaningGroup.definitions.map((definition, index) => (
-                  <li className="def mb-3" key={index}>{definition.definition}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          ))}
+            {/* Footer */}
+            <section className="footer" >
+              < div className="sources-title font-inter font-regular text-lightGrey mb-1 underline decoration-solid decoration-lightGrey decoration-2">Source</div>
+              <div className="sources font-inter font-regular text-navyBlack underline decoration-solid decoration-darkGrey dark-text">{source}
+              </div>
+            </section >
 
-          {/* Synonyms */}
-          <div className="synonyms-container flex items-left gap-7">
-            <div className="synonyms-header font-inter font-regular text-lightGrey mb-3">Synonyms</div>
-            <div className="synonyms font-inter text-purple font-bold">electronic keyboard</div>
-          </div>
-        </div>
+          </>
+        )}
       </section >
-
-      {/* Footer */}
-      <section className="footer" >
-        <div className="sources-title">Source</div>
-        <div className="sources">https://en.wiktionary.org/wiki/keyboard
-        </div>
-      </section >
+      {/* {meanings[0]?.synonyms?.map((synonym, index) => (
+                    <span
+                      key={index}
+                      className="synonyms font-inter text-purple font-bold"
+                      onClick={() => changeWordHandler(synonym)}
+                    >
+                      {synonym}
+                    </span>
+                  ))} */}
     </div >
   )
 }
