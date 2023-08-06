62498dcd0fa8cbf3180027306efcda7d3e6dfce9
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-08-06 11:04:01 -0500

    Edite vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-});
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [svgr(), react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/dictionary-app/'
+  }
+
+  return config
+})
